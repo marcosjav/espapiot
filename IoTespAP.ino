8dfa52126238414e4520b17d7c09d9a99a3b5fad
Marcos Vazquez <mjv87ster@gmail.com>
2019-03-18 09:26:13 -0300

    comments

--- IoTespAP.ino
+++ IoTespAP.ino
@@ -1,5 +1,7 @@
 /*
    starting from the WiFi example
+   This program creates a WiFi AP for select and connect to a existing WiFi. Saving in EEPROM the selected SSID (32bytes) and the current pass (64bytes)
+   Once is internet connection estabished, it will comunicate with the cloud server and create a local network server no do stuff
 */
 
 /* Create a WiFi access point and provide a web server on it. */
@@ -124,5 +126,40 @@ String showHTTP(){
   return html;
 }
 
+/*
+ * EEPROM EXAMPLE
+ 
+#include <EEPROM.h>
 
+// the current address in the EEPROM (i.e. which byte
+// we're going to write to next)
+int addr = 0;
+
+ void setup() {
+  EEPROM.begin(512);
+}
+
+void loop() {
+  // need to divide by 4 because analog inputs range from
+  // 0 to 1023 and each byte of the EEPROM can only hold a
+  // value from 0 to 255.
+  int val = analogRead(A0) / 4;
+
+  // write the value to the appropriate byte of the EEPROM.
+  // these values will remain there when the board is
+  // turned off.
+  EEPROM.write(addr, val);
+
+  // advance to the next address.  there are 512 bytes in
+  // the EEPROM, so go back to 0 when we hit 512.
+  // save all changes to the flash.
+  addr = addr + 1;
+  if (addr == 512) {
+    addr = 0;
+    EEPROM.commit();
+  }
+
+  delay(100);
+}
+ */
 
